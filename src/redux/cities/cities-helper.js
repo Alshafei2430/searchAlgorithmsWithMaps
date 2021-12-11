2bb2ee5e342a81a78fb5276b9a04b49662179d4c
Alshafei2430 <alshafei.2430@gmail.com>
2021-12-11 20:56:09 +0200

    that shit url again

--- src/redux/cities/cities-helper.js
+++ src/redux/cities/cities-helper.js
@@ -1,5 +1,5 @@
 export const handleGetCities = async () => {
-    const response = await fetch('http://localhost:5000/cities')
+    const response = await fetch('https://algorithms-api.herokuapp.com/cities')
     .then(response => response.json())
     .then(({data: citiesList}) => citiesList)
     return response
@@ -7,7 +7,7 @@ export const handleGetCities = async () => {
 
 export const handleGetPathCities = async (payload) => {
     const {algo, endCity, startCity} = payload
-    const response = await fetch(`http://localhost:5000/${algo}`, {
+    const response = await fetch(`https://algorithms-api.herokuapp.com/${algo}`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
